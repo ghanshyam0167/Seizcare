c52f113ca08f5980ddcafd69c56b184f1b4933b4
ghanshyam0167 <ghanshyam0167.be23@chitkara.edu.in>
2025-11-19 09:50:22 +0530

    Initializing Project

---
+++ Seizcare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seizcare
+//
+//  Created by Student on 19/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
