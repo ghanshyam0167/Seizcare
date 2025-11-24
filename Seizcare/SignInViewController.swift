dd3714043449837a6273aff9283839490431c0b0
anmol4real <anmolpreet2172005@gmail.com>
2025-11-24 18:54:39 +0530

    Adding sign in and sign up screens

---
+++ Seizcare/SignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInViewController.swift
+//  Seizcare
+//
+//  Created by Student on 24/11/25.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
